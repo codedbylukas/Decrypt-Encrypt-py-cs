4d1e63bf7b13b0ed3eb907ecad63af7e928be79c
codedbylukas <lukaswittek96@gmail.com>
2026-05-25 07:23:16 +0200

    add

---
+++ cs/DecpyptFile/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SecureFileDecryptionNamespace
+{
+    public static class SecureFileDecryption
+    {
+        public static void DecryptFileInPlace(string path, string password, string SaltString)
+        {
+            byte[] salt = Encoding.UTF8.GetBytes(SaltString);
+            using (Rfc2898DeriveBytes keyDerivation = new(password, salt, 10000, HashAlgorithmName.SHA256))
+            {
+                byte[] key = keyDerivation.GetBytes(32); // 256 Bits
+                byte[] iv = keyDerivation.GetBytes(16);  // 128 Bit
+                byte[] originalData = File.ReadAllBytes(path);
+                byte[] decryptedData;
+                using (Aes aes = Aes.Create())
+                {
+                    aes.Key = key;
+                    aes.IV = iv;
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        using (CryptoStream cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Write))
+
+                        {
+                            cs.Write(originalData, 0, originalData.Length);
+                        }
+                        decryptedData = ms.ToArray();
+                    }
+                }
+                File.WriteAllBytes(path, decryptedData);
+            }
+        }
+    }
+}
