7197e05db8f3e1844e7a3c60e7281b40b64ee47d
codedbylukas <lukaswittek@gmail.com>
2026-05-25 00:42:45 -0500

    add

--- cs/module.py
+++ cs/module.py
@@ -8,16 +8,34 @@ load("coreclr")
 import clr
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
-dll_path = os.path.join(current_dir, "EncryptFile.dll")
-clr.AddReference(dll_path)
+dll_path_enc = os.path.join(current_dir, "EncryptFile.dll")
+dll_path_dec = os.path.join(current_dir, "DecryptFile.dll")
+clr.AddReference(dll_path_enc)
+clr.AddReference(dll_path_dec)
 
 from SecureFileEncryptionNamespace import SecureFileEncryption
+from SecureFileDecryptionNamespace import SecureFileDecryption
 
 
 def encrypt_file(file_path: Path, password: str, salt: str):
     try:
         print("[*] Rufe C#-Verschlüsselung auf...")
         SecureFileEncryption.EncryptFileInPlace(str(file_path), password, salt)
+        print("[+] Erfolgreich ausgeführt.")
+    except KeyboardInterrupt:
+        choice = Confirm.ask("Do you want to exit?")
+        if choice:
+            print("Exiting...")
+            exit(0)
+        else:
+            print("Continuing execution...")
+    except Exception as e:
+        print(f"Error. This goes wrong: {e}")
+
+def decrypt_file(file_path: Path, password: str, salt: str):
+    try:
+        print("[*] Rufe C#-Entschlüsselung auf...")
+        SecureFileDecryption.DecryptFileInPlace(str(file_path), password, salt)
         print("[+] Erfolgreich ausgeführt.")
     except KeyboardInterrupt:
         choice = Confirm.ask("Do you want to exit?")
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from pathlib import Path
-from cs.module import encrypt_file
+from rich.prompt import Confirm
+
+from cs.module import encrypt_file, decrypt_file
 from py_src.choice import ask_for_file_or_folder, ask_encryption_or_decryption, ask_for_password, ask_for_salt, ask_file_or_folder_file_path
 from py_src.encrypt_folder import encrypt_folder
 
@@ -9,20 +11,27 @@ encrypting: bool = None
 is_file: bool = None
 
 def main():
-    global encrypting, is_file
-    encrypting = ask_encryption_or_decryption(encrypting)
-    used_path = ask_file_or_folder_file_path()
-    is_file = ask_for_file_or_folder(is_file)
-    if encrypting:
-        if is_file:
-            encrypt_file(used_path, PASSWORD, SALT)
-        elif not is_file:
-            encrypt_folder(used_path, PASSWORD, SALT)
-    elif not encrypting: # Decrypting
-        if is_file:
-            print("File decryption is not yet implemented.")
-        elif not is_file:
-            print("Folder decryption is not yet implemented.")    
+    while True:
+        global encrypting, is_file
+        encrypting = ask_encryption_or_decryption(encrypting)
+        used_path = ask_file_or_folder_file_path()
+        is_file = ask_for_file_or_folder(is_file)
+        if encrypting:
+            if is_file:
+                encrypt_file(used_path, PASSWORD, SALT)
+            elif not is_file:
+                encrypt_folder(used_path, PASSWORD, SALT)
+        elif not encrypting: # Decrypting
+            if is_file:
+                decrypt_file(used_path, PASSWORD, SALT)
+            elif not is_file:
+                print("Folder decryption is not yet implemented.")    
+        continue_choice = Confirm.ask("Do you want to continue?", default=True)
+        if not continue_choice:
+            print("Exiting the program. Goodbye!")
+            break
+        else:
+            print("Continuing the program...")
 
 if __name__ == "__main__":
     main()
