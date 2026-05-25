c8fbe4fa80a451878df342c66042dc02160330fd
codedbylukas <lukaswittek@gmail.com>
2026-05-25 00:00:33 -0500

    adding

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+from pathlib import Path
 from cs.module import encrypt_file
 from py_src.choice import ask_for_file_or_folder, ask_encryption_or_decryption, ask_for_password, ask_for_salt, ask_file_or_folder_file_path
+from py_src.encrypt_folder import encrypt_folder
 
-PASSWORD = ask_for_password()
-SALT = ask_for_salt()
+PASSWORD: str = ask_for_password()
+SALT: str = ask_for_salt()
 encrypting: bool = None
 is_file: bool = None
 
@@ -15,8 +17,8 @@ def main():
         if is_file:
             encrypt_file(used_path, PASSWORD, SALT)
         elif not is_file:
-            print("Folder encryption is not yet implemented.")
-    elif not encrypting:
+            encrypt_folder(used_path, PASSWORD, SALT)
+    elif not encrypting: # Decrypting
         if is_file:
             print("File decryption is not yet implemented.")
         elif not is_file:
