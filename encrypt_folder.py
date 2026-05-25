92994b45f89ec7bc8f4f67468b2c0ea75c2e1a71
codedbylukas <lukaswittek@gmail.com>
2026-05-24 23:56:19 -0500

    add

---
+++ encrypt_folder.py
@@ -0,0 +1,19 @@
+import os
+from pathlib import Path
+from cs.module import encrypt_file
+
+def get_all_files(directory: Path,  file_paths: list) -> list:
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            file_paths.append(os.path.join(root, file))
+    return file_paths
+
+def encrypt_folder(folder_path: Path, password: str, salt: str) -> None:
+    file_paths = []
+    file_paths = get_all_files(folder_path, file_paths)
+    for file_path in file_paths:
+        encrypt_file(file_path, password, salt)
+
+if __name__ == "__main__":
+    folder_path = Path("./a")
+    encrypt_folder(folder_path, "password", "salt")
