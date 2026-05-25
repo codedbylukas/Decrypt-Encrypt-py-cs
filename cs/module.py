d75338b08d0c7a550353d123104d2be1a3784ba2
codedbylukas <lukaswittek@gmail.com>
2026-05-24 23:25:19 -0500

    add

--- cs/module.py
+++ cs/module.py
@@ -8,7 +8,7 @@ load("coreclr")
 import clr
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
-dll_path = os.path.join(current_dir, "DecryptFile.dll")
+dll_path = os.path.join(current_dir, "EncpyptFile.dll")
 clr.AddReference(dll_path)
 
 from SecureFileEncryptionNamespace import SecureFileEncryption
