6605d63b2769eb93e0775c75ccb5b1befa2b70c8
codedbylukas <lukaswittek@gmail.com>
2026-05-24 23:00:19 -0500

    add

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from cs.module import encrypt_file
 
+PASSWORD = "dein_passwort"
+SALT = "dein_salt"
+
+
+
 if __name__ == "__main__":
-    encrypt_file("a.txt", "dein_passwort", "dein_salt")
+    encrypt_file("a.txt", PASSWORD, SALT)
