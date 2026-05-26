91c2aed2ebdb432858009231357e29d7a6fb56f3
codedbylukas <lukaswittek@gmail.com>
2026-05-26 03:56:04 -0500

    add

--- cs/DecpyptFile/main.cs
+++ cs/DecpyptFile/main.cs
@@ -7,30 +7,41 @@ namespace SecureFileDecryptionNamespace
 {
     public static class SecureFileDecryption
     {
-        public static void DecryptFileInPlace(string path, string password, string SaltString)
+        public static void DecryptFileInPlace(string path, string password, string saltString)
         {
-            byte[] salt = Encoding.UTF8.GetBytes(SaltString);
-            using (Rfc2898DeriveBytes keyDerivation = new(password, salt, 10000, HashAlgorithmName.SHA256))
+            try
             {
-                byte[] key = keyDerivation.GetBytes(32); // 256 Bits
-                byte[] iv = keyDerivation.GetBytes(16);  // 128 Bit
-                byte[] originalData = File.ReadAllBytes(path);
-                byte[] decryptedData;
-                using (Aes aes = Aes.Create())
+                byte[] salt = Encoding.UTF8.GetBytes(saltString);
+                
+                using (Rfc2898DeriveBytes keyDerivation = new Rfc2898DeriveBytes(password, salt, 10000, HashAlgorithmName.SHA256))
                 {
-                    aes.Key = key;
-                    aes.IV = iv;
-                    using (MemoryStream ms = new MemoryStream())
-                    {
-                        using (CryptoStream cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Write))
+                    byte[] key = keyDerivation.GetBytes(32); // 256 Bits
+                    byte[] iv = keyDerivation.GetBytes(16);  // 128 Bits
+                    byte[] originalData = File.ReadAllBytes(path);
+                    byte[] decryptedData;
 
+                    using (Aes aes = Aes.Create())
+                    {
+                        aes.Key = key;
+                        aes.IV = iv;
+
+                        using (MemoryStream ms = new MemoryStream())
                         {
-                            cs.Write(originalData, 0, originalData.Length);
+                            using (CryptoStream cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Write))
+                            {
+                                cs.Write(originalData, 0, originalData.Length);
+                            }
+                            decryptedData = ms.ToArray();
                         }
-                        decryptedData = ms.ToArray();
                     }
+
+                    File.WriteAllBytes(path, decryptedData);
                 }
-                File.WriteAllBytes(path, decryptedData);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error occurred while decrypting file: {ex.Message}");
+                throw;
             }
         }
     }
